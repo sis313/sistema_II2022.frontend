968b03c28e207df70c1ec7fa926ef48a40c324b1
GaboMV <gabo171999@gmail.com>
2022-09-06 18:45:19 -0700

    Interfaz de login y registro

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  userlogin = true;
+  userregister = false;
+  //Buttons clicks functionalities
+  user_register()
+  {
+    this.userlogin = false;
+    this.userregister = true;
+  }
+  user_login()
+  {
+    this.userlogin = true;
+    this.userregister = false;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/verificacion/verificacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verificacion',
+  templateUrl: './verificacion.component.html',
+  styleUrls: ['./verificacion.component.css']
+})
+export class VerificacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
