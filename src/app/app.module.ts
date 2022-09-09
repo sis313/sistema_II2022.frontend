de14f4b9f977996faf1dce4e5f7883597e2b2412
GaboMV <gabo171999@gmail.com>
2022-09-08 18:06:08 -0700

    arreglando carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './view/login/login.component';
 import {FormsModule} from "@angular/forms";
-import { VerificacionComponent } from './verificacion/verificacion.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    VerificacionComponent
+
   ],
   imports: [
     BrowserModule,
