9e74c3da4309f24873c87c7a59fc4d1ca07d5b24
Ricardo16500 <fabiorvm16500@gmail.com>
2022-09-11 19:44:50 -0400

    add homeComponent and routerLink

--- ClientsView/src/app/app.module.ts
+++ ClientsView/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainListComponent } from './View/main-list/main-list.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HomeComponentComponent } from './View/home-component/home-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainListComponent
+    MainListComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponentComponent},
+      {path: 'main', component: MainListComponent},
+      {path: '',redirectTo: '/home', pathMatch: 'full'}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
