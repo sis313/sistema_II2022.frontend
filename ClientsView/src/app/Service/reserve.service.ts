3c0556aeb2a03c697c30a5896bfbdca936effa7f
Ricardo16500 <fabiorvm16500@gmail.com>
2022-09-29 18:31:35 -0400

    change data in reserve.service

--- ClientsView/src/app/Service/reserve.service.ts
+++ ClientsView/src/app/Service/reserve.service.ts
@@ -1,11 +1,24 @@
 import  { Injectable } from "@angular/core";
-
+import { Reserve } from "../Model/reserve.model";
+import { HttpClient } from "@angular/common/http";
 @Injectable({ 
     providedIn: 'root',
 })
 
 export class ReserveService{
-    constructor(){
+    reserve: Reserve[] = [];
+    constructor(private http: HttpClient) {
+        }
+    setReserve(data: Reserve[]){
+        this.reserve = data;
+    }
+    getReserve(){
+        return this.reserve;
+    }
+    getReserveHttp(){
+
+    }
+    setReserveHttp(){
         
     }
 }
