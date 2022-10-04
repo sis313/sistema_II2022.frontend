6e146fb6a319651d0db3b2e1b4873819644ddc1c
Ricardo16500 <fabiorvm16500@gmail.com>
2022-10-03 20:58:40 -0400

    change comentary service

--- ClientsView/src/app/Service/comentary.service.ts
+++ ClientsView/src/app/Service/comentary.service.ts
@@ -1,11 +1,27 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Comentary } from "../Model/comentary.model";
+
 
 @Injectable({   
     providedIn: 'root',
 })
 
 export class ComentaryService{
-    constructor(){
+    comentary: Comentary[] = [];
+    constructor(private http: HttpClient){
+        
+    }
+    setComentary(data: Comentary[]){
+        this.comentary = data;
+    }
+    getComentary(){
+        return this.comentary;
+    }
+    setComentaryHttp(){
+
+    }
+    GetComentaryHttp(){
         
     }
 }
