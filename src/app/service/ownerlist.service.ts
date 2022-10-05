9e07970290dde3cb1de2680b54f54e6a4b9313f5
JuancaJcA <avilafloresjc@gmail.com>
2022-10-05 12:37:03 -0400

    Delete owner button working

--- src/app/service/ownerlist.service.ts
+++ src/app/service/ownerlist.service.ts
@@ -5,16 +5,20 @@ import { environment } from 'src/environments/environment';
 import { user } from '../model/User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OwnerlistService {
+  private baseUrl: string = environment.baseUrl;
+  constructor(private http: HttpClient) {}
 
-  private baseUrl:string = environment.baseUrl;
-  constructor(private http:HttpClient){ }
-
-  getListOwner():Observable<user[]> {
+  getListOwner(): Observable<user[]> {
     const url = `${this.baseUrl}user/type=2/status=1`;
     console.log(url);
     return this.http.get<user[]>(url);
+  }
+
+  deleteOwner(idOwner: number): Observable<void> {
+    const url = `${this.baseUrl}/user/deleteOwner/${idOwner}`;
+    return this.http.delete<void>(url);
   }
 }
