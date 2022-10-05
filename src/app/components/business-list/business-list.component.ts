9a90c2298139b22c30219a197cc0b8b3739c62ee
JuancaJcA <avilafloresjc@gmail.com>
2022-10-05 11:53:26 -0400

    Changes in business list :D

--- src/app/components/business-list/business-list.component.ts
+++ src/app/components/business-list/business-list.component.ts
@@ -13,49 +13,24 @@ import { BusinesslistService } from 'src/app/service/businesslist.service';
   styleUrls: ['./business-list.component.css'],
 })
 export class BusinessListComponent implements OnInit {
-  data: business[] = [];
-  columnsToDisplay: string[] = ['id', 'name', 'description','edit','delete'];
+  inactive:business[] = [];
   businessList:business[] = [];
-  displayedColumns: string[] = ['id', 'name', 'description','edit','delete'];
-  dataSource = new MatTableDataSource();
-  cad!:string;
-
-  @ViewChild(MatPaginator, {static:true}) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
 
   constructor(private _liveAnnouncer: LiveAnnouncer,private businessListService: BusinesslistService,private router: Router) {}
 
   async ngOnInit(): Promise<void> {
 
-      this.businessList = await this.getAdminData();
-      this.dataSource.data = this.businessList;
+    this.businessList = await this.getBusinessData();
+    
   }
 
-  async getAdminData(){
+  async getBusinessData(){
     let respuesta!:business[];
     await this.businessListService.getListBusiness().toPromise().then((response) => {
       respuesta = response;
     }).catch(e => console.error(e));
 
     return respuesta;
-  }
-
-  ngAfterViewInit(){
-    this.dataSource.paginator= this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
-  announceSortChange(sortState: Sort) {
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
   }
   
   newChange(): void {
