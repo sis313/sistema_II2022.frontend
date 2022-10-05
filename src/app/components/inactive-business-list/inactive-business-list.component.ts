f475b05b3625c8b702a1611748b977f2f98b19b5
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2022-10-05 11:29:48 -0400

    fix: Improve inactive business list interface

--- src/app/components/inactive-business-list/inactive-business-list.component.ts
+++ src/app/components/inactive-business-list/inactive-business-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from'sweetalert2';
 import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import { business } from 'src/app/model/Business';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { BusinesslistService } from 'src/app/service/businesslist.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inactive-business-list',
@@ -9,9 +13,24 @@ import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 })
 export class InactiveBusinessListComponent implements OnInit {
 
-  constructor() { }
+  inactive:business[] = [];
+  businessList:business[] = [];
 
-  ngOnInit(): void {
+  constructor(private _liveAnnouncer: LiveAnnouncer,private businessListService: BusinesslistService,private router: Router) {}
+
+  async ngOnInit(): Promise<void> {
+
+    this.businessList = await this.getInactiveBusinessData();
+    
+  }
+
+  async getInactiveBusinessData(){
+    let respuesta!:business[];
+    await this.businessListService.getListInactiveBusiness().toPromise().then((response) => {
+      respuesta = response;
+    }).catch(e => console.error(e));
+
+    return respuesta;
   }
 
 }
