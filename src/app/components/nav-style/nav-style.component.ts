ad5f0fdc5c12c43247fd0bbac69a8d435709bf17
Noelia Vasquez Crespo <noelia.vasquez@ucb.edu.bo>
2022-10-20 16:26:03 -0400

    add: creating new nav

--- src/app/components/nav-style/nav-style.component.ts
+++ src/app/components/nav-style/nav-style.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-style',
   templateUrl: './nav-style.component.html',
-  styleUrls: ['./nav-style.component.css']
+  styleUrls: ['./nav-style.component.scss']
 })
 export class NavStyleComponent implements OnInit {
 
