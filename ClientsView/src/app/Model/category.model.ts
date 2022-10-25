41518027f8c6cb753efa5802e7e19251a2da7a04
Ignacio Paz <natxio.dev@gmail.com>
2022-10-24 21:57:18 -0400

    add: category model and service

---
+++ ClientsView/src/app/Model/category.model.ts
@@ -0,0 +1,12 @@
+export class Category {
+    constructor(
+      public idTypeBusiness: number,
+      public name: string,
+   
+    ) {
+      this.idTypeBusiness = idTypeBusiness;
+      this.name = name;
+     
+    }
+  }
+  
---
+++ ClientsView/src/app/Service/category.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Category } from '../Model/category.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BranchService {
+  category: Category[] = [];
+  categoryTemp: Category[] = [];
+  constructor(private http: HttpClient) {}
+
+  setCategoryName(data: Category[]) {
+    this.category = data;
+  }
+
+  getCategoryName() {
+    return this.category;
+  }
+  getAllCategories() {
+    return this.http.get<any>(
+      'https://serviceprojectspring.herokuapp.com/api/typeBusiness'
+    );
+  }
+}
